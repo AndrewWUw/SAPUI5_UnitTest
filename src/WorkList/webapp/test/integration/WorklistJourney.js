3a6ae4148f5a3f7f71e58cc8ca0a214db6fa9469
Andrew WU <andrew_wu@outlook.com>
2017-12-22 16:46:59 +1100

    Init for integration test

--- src/WorkList/webapp/test/integration/WorklistJourney.js
+++ src/WorkList/webapp/test/integration/WorklistJourney.js
@@ -1,24 +1,36 @@
-/*global QUnit*/
+/* global QUnit */
+sap.ui.require([
+    "sap/ui/test/opaQunit"
+  ],
+  function(opaTest) {
+    "use strict";
 
-sap.ui.require(
-	["sap/ui/test/opaQunit"],
-	function (opaTest) {
-		"use strict";
+    QUnit.module("Posts");
 
-		QUnit.module("Posts");
+    opaTest("Should see the table with all Posts", function(Given, When, Then) {
+      // Arrangements
+      Given.iStartMyApp();
 
-		opaTest("Should see the table with all Posts", function (Given, When, Then) {
-			// Arrangements
-			Given.iStartMyApp();
+      //Actions
+      When.onTheWorklistPage.iLookAtTheScreen();
 
-			//Actions
-			When.onTheWorklistPage.iLookAtTheScreen();
+      // Assertions
+      // Then.onTheWorklistPage.theTableShouldHaveAllEntries().
+      // and.theTitleShouldDisplayTheTotalAmountOfItems().
+      // and.iTeardownMyAppFrame();
 
-			// Assertions
-			Then.onTheWorklistPage.theTableShouldHaveAllEntries().
-				and.theTitleShouldDisplayTheTotalAmountOfItems().
-				and.iTeardownMyAppFrame();
-		});
+      Then.onTheWorklistPage.theTableShouldHavePagination().
+      and.theTitleShouldDisplayTheTotalAmountOfItems();
+    });
 
-	}
+    opaTest("Should be able to load more items", function(Given, When, Then) {
+      //Actions
+      When.onTheWorklistPage.iPressOnMoreData();
+      // Assertions
+
+      Then.onTheWorklistPage.theTableShouldHaveAllEntries().
+      and.iTeardownMyAppFrame();
+    });
+
+  }
 );
--- src/WorkList/webapp/test/integration/pages/Common.js
+++ src/WorkList/webapp/test/integration/pages/Common.js
@@ -1,40 +1,42 @@
 sap.ui.define([
-		'sap/ui/test/Opa5'
-	],
-	function (Opa5) {
-		"use strict";
+    'sap/ui/test/Opa5'
+  ],
+  function(Opa5) {
+    "use strict";
 
-		function getFrameUrl(sHash, sUrlParameters) {
-			sHash = sHash || "";
-			var sUrl = jQuery.sap.getResourcePath("sap/ui/demo/bulletinboard/app", ".html");
+    function getFrameUrl(sHash, sUrlParameters) {
+      sHash = sHash || "";
+      var sUrl = jQuery.sap.getResourcePath("sap/ui/demo/bulletinboard/app", ".html");
 
-			if (sUrlParameters) {
-				sUrlParameters = "?" + sUrlParameters;
-			}
+      if (sUrlParameters) {
+        sUrlParameters = "?" + sUrlParameters;
+      }
 
-			return sUrl + sUrlParameters + "#" + sHash;
-		}
+      return sUrl + sUrlParameters + "#" + sHash;
+    }
 
-		return Opa5.extend("sap.ui.demo.bulletinboard.test.integration.pages.Common", {
+    return Opa5.extend("sap.ui.demo.bulletinboard.test.integration.pages.Common", {
 
-			constructor: function (oConfig) {
-				Opa5.apply(this, arguments);
+      constructor: function(oConfig) {
+        Opa5.apply(this, arguments);
 
-				this._oConfig = oConfig;
-			},
+        this._oConfig = oConfig;
+      },
 
-			iStartMyApp: function (oOptions) {
-				var sUrlParameters;
-				oOptions = oOptions || { delay: 0 };
+      iStartMyApp: function(oOptions) {
+        var sUrlParameters;
+        oOptions = oOptions || {
+          delay: 0
+        };
 
-				sUrlParameters = "serverDelay=" + oOptions.delay;
+        sUrlParameters = "serverDelay=" + oOptions.delay;
 
-				this.iStartMyAppInAFrame(getFrameUrl(oOptions.hash, sUrlParameters));
-			},
+        this.iStartMyAppInAFrame(getFrameUrl(oOptions.hash, sUrlParameters));
+      },
 
-			iLookAtTheScreen: function () {
-				return this;
-			}
+      iLookAtTheScreen: function() {
+        return this;
+      }
 
-		});
-	});
+    });
+  });
